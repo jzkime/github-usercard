933f09d7764e8d8a7e3e3b62a870dd76e0f2352d
Jaz Kime <JazDKime@gmail.com>
2022-05-12 14:05:49 -0700

    Github Cards Functionality Complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -28,7 +29,69 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["tim-sansone", "ashurik522", "gretchyn-hickman", "christinebussingerdev", "Abargallo19", "RealSadLemon", "FytaiLynch"];
+
+function getGitHub(obj){
+  const divCard = document.createElement("div");
+  const imgIcon = document.createElement("img");
+  const divInfo = document.createElement("div");
+  const nameH = document.createElement("h3");
+  const userP = document.createElement("p");
+  const locaP = document.createElement("p");
+  const profileP = document.createElement("p");
+  const profileA = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+
+  divCard.classList.add("card");
+  imgIcon.src = obj.avatar_url;
+  divInfo.classList.add("card-info");
+  nameH.classList.add("name");
+  nameH.textContent = obj.name
+  userP.classList.add("username");
+  userP.textContent = obj.login;
+  locaP.textContent = `Location: ${obj.location}`;
+  profileP.textContent = `Profile: `;
+  profileA.href = obj.html_url;
+  profileA.textContent = obj.html_url;
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = obj.bio;
+
+  divCard.appendChild(imgIcon);
+  divCard.appendChild(divInfo);
+  divInfo.appendChild(nameH);
+  divInfo.appendChild(userP);
+  divInfo.appendChild(locaP);
+  divInfo.appendChild(profileP);
+  profileP.appendChild(profileA);
+  divInfo.appendChild(followers);
+  divInfo.appendChild(following);
+  divInfo.appendChild(bio);
+
+  return divCard;
+}
+
+const cards = document.querySelector(".cards")
+
+// api is able to get all the data needed = own function, can put any username through;
+function cardMaker(user) {
+  axios.get(`https://api.github.com/users/${user}`)
+  .then((res) => {
+  const finalCard = getGitHub(res.data);
+  cards.appendChild(finalCard);
+})
+.catch((err) => {
+  console.log(err)
+})
+}
+
+cardMaker("jzkime")
+followersArray.forEach(user => {
+  cardMaker(user);
+})
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
